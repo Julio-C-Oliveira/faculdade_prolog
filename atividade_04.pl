d45b7ce719ef2198e02f08c2e68ae6c21a564719
Julio-C-Oliveira <julio3.oliveir@gmail.com>
2025-07-05 10:45:04 -0300

    refactor(atividade04): Retira todos os exemplos adicionados ao arquivo.

--- atividade_04.pl
+++ atividade_04.pl
@@ -1,39 +1,3 @@
-% Testes Aleatorios:
-
-/*
-    - funcao(input, output).
-    - [Head|Tail] O pipe define a separação entre o que vai pra cabeça e o que vai pra cauda, no exemplo só um elemento está sendo extraido. 
-    -
-*/
-head([Head|_], Head). % Função pra pegar o primeiro elemento.
-?- head([0,1,2,3,4,5], X) % Teste, retorna 0.
-
-tail([_|Tail], Tail). % Função para pegar todos os elementos exceto o primeiro.
-?- tail([0,1,2,3,4,5], X) % Retorna [1,2,3,4,5]
-
-sum([], 0). % Caso base
-sum([Head|Tail], FinalSum) :- % Define o que recebe e o que retorna.
-    sum(Tail, PartialSum), % Define a chamada recursiva.
-    FinalSum is PartialSum + Head. % Define o que é o FinalSum e adiciona a lógica da recursão. soma é igual a cabeça + a soma da cauda.
-
-?. sum([1,2,3,4,5], X). % Retorna 15
-
-getLenght([], 0). % Caso base, não deu pra usar o nome lenght porque é reservado.
-getLenght([_|Tail], FinalLenght) :- % O Head é uam váriavel anônima, pois seu valor não é necessário.
-    getLenght(Tail, PartialLenght), % Define como é a chamada recursiva.
-    FinalLenght is 1 + PartialLenght. % Define a lógica da recursão. Tamanho é igual a 1 + o tamanho da cauda.
-
-?. getLenght([1,2,3,4,5], X). % Retorna 5
-
-containsElement([], false). % Em caso de lista vazia, retorna false.
-containsElement([Head|_], Head). % Compara o primeiro elemento da lista com o elemento que está sendo buscado, o nome é o mesmo para fazer a comparação.
-containsElement([_|Tail], FinalResult) :- 
-    containsElement(Tail, FinalResult).
-
-?. containsElement([1,2,3,4,5], 2). % Retorna true
-
-% Funções Gerais:
-
 % Q - 01: Regra para verificar a frequência de um elemento na lista.
 
 % Q - 02: Regra para concatenar duas listas.
@@ -53,20 +17,3 @@ containsElement([_|Tail], FinalResult) :-
 % Q - 09: Regra para informar os elementos duplicados, considerando a ordem em que eles aparecem na lista.
 
 % Q - 10: Regra para eliminar os elementos duplicados.
-
-/*
-    Buscas Simples de Cabeça e Cauda:
-    [Head | Tail] = [mia, vicent, yolanda, bob] 
-        - Head = mia, 
-        - Tail = [vicent, yolanda, bob]
-    [Head1, Head2 | Tail] = [mia, vicent, yolanda, bob] 
-        - Head1 = mia, 
-        - Head2 = vicent, 
-        - Tail = [yolanda, bob]
-    [_, Head2 | Tail] = [mia, vicent, yolanda, bob]
-        - Head2 = vicent,
-        - Tail = [yolanda, bob]
-
-    Soma Recursiva:
-    
-*/
