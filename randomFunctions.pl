22eb9159a8eaf09e303c965f95218b7d836c706e
Julio-C-Oliveira <julio3.oliveir@gmail.com>
2025-07-05 21:18:30 -0300

    feat(randomFunctions): Adiciona soma de elementos de várias listas, +
    Além de adicionar essa função de soma, refatora algumas funções para
    facilitar a legibilidade, e altera a descrição das funções.

--- randomFunctions.pl
+++ randomFunctions.pl
@@ -6,6 +6,7 @@
     - -> essa setá representa então, se algo for verdade, faça isso.
     - ; esse ponto e virgula representa o senão.
     - , essa virgula representa o and.
+    - :- significa é verdadeiro se, serve para definir clausulas.
 */
 
 head([Head|_], Head). % Função pra pegar o primeiro elemento.
@@ -16,24 +17,24 @@ tail([_|Tail], Tail). % Função para pegar todos os elementos exceto o primeiro
 
 sum([], 0). % Caso base
 sum([Head|Tail], FinalSum) :- % Define o que recebe e o que retorna.
-    sum(Tail, PartialSum), % Define a chamada recursiva.
-    FinalSum is PartialSum + Head. % Define o que é o FinalSum e adiciona a lógica da recursão. soma é igual a cabeça + a soma da cauda.
+    sum(Tail, TailSum), % Define a chamada recursiva.
+    FinalSum is TailSum + Head. % Define o que é o FinalSum e adiciona a lógica da recursão. soma é igual a cabeça + a soma da cauda.
 
 % ?. sum([], X). % Retorna 0
 % ?. sum([1,2,3,4,5], X). % Retorna 15
 
 getLenght([], 0). % Caso base, não deu pra usar o nome lenght porque é reservado.
 getLenght([_|Tail], FinalLenght) :- % O Head é uam váriavel anônima, pois seu valor não é necessário.
-    getLenght(Tail, PartialLenght), % Define como é a chamada recursiva.
-    FinalLenght is 1 + PartialLenght. % Define a lógica da recursão. Tamanho é igual a 1 + o tamanho da cauda.
+    getLenght(Tail, TailLenght), % Define como é a chamada recursiva.
+    FinalLenght is 1 + TailLenght. % Define a lógica da recursão. Tamanho é igual a 1 + o tamanho da cauda.
 
 % ?. getLenght([], X). % Retorna 0
 % ?. getLenght([1,2,3,4,5], X). % Retorna 5
 
 containsElement([], false). % Em caso de lista vazia, retorna false.
 containsElement([Head|_], Head). % Compara o primeiro elemento da lista com o elemento que está sendo buscado, o nome é o mesmo para fazer a comparação.
-containsElement([_|Tail], FinalResult) :- 
-    containsElement(Tail, FinalResult).
+containsElement([_|Tail], TailResult) :- 
+    containsElement(Tail, TailResult).
 
 % ?. containsElement([1,2,3,4,5], 0). % Retorna false
 % ?. containsElement([1,2,3,4,5], 1). % Retorna true
@@ -42,10 +43,10 @@ containsElement([_|Tail], FinalResult) :-
 getElementIndex([], _, -1). % Retorna -1 caso a lista seja vazia.
 getElementIndex([Head|_], Head, 0). % Retorna 0 caso o primeiro elemento seja igual a cabeça da lista.
 getElementIndex([_|Tail], Element, FinalIndex) :- % Chamada recursiva.
-    getElementIndex(Tail, Element, PartialIndex), % Passo recursivo.
+    getElementIndex(Tail, Element, TailIndex), % Passo recursivo.
     (
-    	PartialIndex == -1 ->  FinalIndex = -1; % Caso o valor seja -1, para propagar a inexistência do item.
-    	PartialIndex >= 0 -> FinalIndex is PartialIndex + 1 % Caso seja maior ou igual a zero para indicar que existe e retornar o valor correto.
+    	TailIndex == -1 ->  FinalIndex = -1; % Caso o valor seja -1, para propagar a inexistência do item.
+    	TailIndex >= 0 -> FinalIndex is TailIndex + 1 % Caso seja maior ou igual a zero para indicar que existe e retornar o valor correto.
     ).
 
 % ?. getElementIndex([1,2,3,4,5], 0, X). % Retorna -1 
@@ -68,8 +69,8 @@ maxElement([Head|Tail], MaxElement) :-
 % ?. maxElement([1,2,7,4,5], X). % Retorna 7
 
 concatenate([], SecondaryList, SecondaryList).  % Caso a primeira lista esteja vazia, retorna a segunda lista
-concatenate([Head|Tail], SecondaryList, [Head|PartialConcatenateList]) :- % Pega o primeiro elemento da primeira lista e passa pra lista final.
-    concatenate(Tail, SecondaryList, PartialConcatenateList).  % Reduz o tamanho da lista primária e pega a lista parcialmente concatenada.
+concatenate([Head|Tail], SecondaryList, [Head|TailConcatenateList]) :- % Pega o primeiro elemento da primeira lista e passa pra lista final.
+    concatenate(Tail, SecondaryList, TailConcatenateList).  % Reduz o tamanho da lista primária e pega a lista parcialmente concatenada.
 
 % ?. concatenate([], [1, 2, 3, 4], X). % Retorna [1, 2, 3, 4]
 % ?. concatenate([1, 2, 3, 4], [], X). % Retorna [1, 2, 3, 4]
@@ -77,11 +78,19 @@ concatenate([Head|Tail], SecondaryList, [Head|PartialConcatenateList]) :- % Pega
 
 
 reverseList([], []). % Se a lista for vazia não tem o que inverter
-reverseList([Head|Tail], ReversedList) :- % Se 
+reverseList([Head|Tail], ReversedList) :-
     reverseList(Tail, ReversedTail),
-    concatenate(ReversedTail, [Head], X),
-    ReversedList = X.
+    concatenate(ReversedTail, [Head], X), % Concatena a lista, mas jogando a cabeça pro final.
+    ReversedList = X. % Retorna o resultado.
 
 % ?. reverseList([], X). % Retorna []
 % ?. reverseList([1], X). % Retorna [1]
 % ?. reverseList([1, 2, 3, 4, 5], X). % Retorna [5, 4, 3, 2, 1]
+
+sumListOfLists([], 0). % Se for vazia retorna 0.
+sumListOfLists([HeadList|TailLists], FinalSum) :- 
+    sum(HeadList, SumHeadList),  % Soma a lista que está mna cabeça.
+    sumListOfLists(TailLists, TailListSum),  % Obtém a soma da cauda.
+    FinalSum is SumHeadList + TailListSum. % Junta a soma da cabeça com a da cauda.
+
+% ?. sumListOfLists([[1, 2, 3, 4, 5],[5]], X). % Retorna 20
